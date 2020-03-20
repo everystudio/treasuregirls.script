0b15951b063adf16e76f1f7676abf75ed32fddfe
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-03-20 23:29:21 +0900

    ショップページの作成開始

---
+++ camp/ShopMain.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+using TMPro;
+
+public class ShopMain : MonoBehaviour
+{
+	public Button m_btnMainGem;
+	public Button m_btnMainGold;
+	public Button m_btnMainWeapon;
+	public Button m_btnMainTreasure;
+	public Button m_btnMainScroll;
+	public Button m_btnMainCode;
+
+	public GameObject m_goBannerGemFree;
+	public GameObject m_goBannerGoldPack1;
+	public GameObject m_goBannerGoldPack2;
+	public GameObject m_goBannerGoldPack3;
+	public GameObject m_goBannerWeaponFree;
+	public GameObject m_goBannerWeapon1;
+	public GameObject m_goBannerWeapon2;
+	public GameObject m_goBannerTreasureFree;
+	public GameObject m_goBannerTreasure1;
+	public GameObject m_goBannerTreasure2;
+
+	public Button m_btnGemFree;
+	public Button m_btnGoldPack1;
+	public Button m_btnGoldPack2;
+	public Button m_btnGoldPack3;
+	public Button m_btnWeaponFree;
+	public Button m_btnWeapon1;
+	public Button m_btnWeapon2;
+	public Button m_btnTreasureFree;
+	public Button m_btnTreasure1;
+	public Button m_btnTreasure2;
+
+	public Button m_btnScrollCreate;
+	public Button m_btnScrollUse;
+
+	public TextMeshProUGUI m_txtScrollNumBlue;
+	public TextMeshProUGUI m_txtScrollNumYellow;
+	public TextMeshProUGUI m_txtScrollNumGreen;
+	public TextMeshProUGUI m_txtScrollNumPurple;
+	public TextMeshProUGUI m_txtScrollNumRed;
+
+	public GameObject m_goScrollInfoRoot;
+	public GameObject m_goScrollCreateRoot;
+	public GameObject m_goScrollUseRoot;
+
+	public Button m_btnScroolCreateYellowOne;
+	public Button m_btnScroolCreateYellowTen;
+	public Button m_btnScroolCreateGreenOne;
+	public Button m_btnScroolCreateGreanTen;
+	public Button m_btnScroolCreatePurpleOne;
+	public Button m_btnScroolCreatePurpleTen;
+	public Button m_btnScroolCreateRedOne;
+	public Button m_btnScroolCreateRedTen;
+
+	public Button m_btnScroolUseBlueOne;
+	public Button m_btnScroolUseBlueTen;
+	public Button m_btnScroolUseYellowOne;
+	public Button m_btnScroolUseYellowTen;
+	public Button m_btnScroolUseGreenOne;
+	public Button m_btnScroolUseGreanTen;
+	public Button m_btnScroolUsePurpleOne;
+	public Button m_btnScroolUsePurpleTen;
+	public Button m_btnScroolUseRedOne;
+	public Button m_btnScroolUseRedTen;
+
+}
