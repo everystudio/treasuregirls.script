a40191c0d5b4289af389c93acdc2dd5815e4291e
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-03-31 11:33:57 +0900

    ロングタップ以外はいらんけどね

---
+++ game/ArrowButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowButton : MonoBehaviour
+{
+    public bool is_press;
+
+    public void OnPointerDown()
+    {
+        is_press = true;
+    }
+    public void OnPointerUp()
+    {
+        is_press = false;
+    }
+
+
+}
---
+++ game/ButtonLongtap.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLongtap : UnityEngine.UI.Button
+{
+}
